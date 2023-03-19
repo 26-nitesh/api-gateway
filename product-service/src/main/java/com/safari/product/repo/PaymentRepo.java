9a2639215d3e6d14272c297cef0734ee6de54b68
nitesh.kumar <nkumar@4321#>
2023-03-19 22:39:41 +0530

    ap gateway

--- product-service/src/main/java/com/safari/product/repo/PaymentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.safari.product.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.safari.product.entity.Payment;
-
-public interface PaymentRepo extends JpaRepository<Payment, Integer>{
-
-}
